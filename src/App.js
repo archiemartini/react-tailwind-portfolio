9629af0c7e9a2b78086ee558338dd56394d0d8be
Archie Martin <archie.cgm@gmail.com>
2023-09-18 16:26:09 +0100

    added a theme font to overwrite base html

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div >
       <h1 className="text-4xl font-bold">Hello</h1>
+      <h1 className="text-4xl font-bold font-signature">Great Vibes</h1>
     </div>
   );
 }

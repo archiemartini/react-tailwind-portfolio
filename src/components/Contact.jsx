3ea2b4fdd78408f32d374b56eed5a26695ecff1e
Archie Martin <archie.cgm@gmail.com>
2023-09-29 11:32:11 +0100

    Link working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full hscreen bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
-        <div className='flex'>
-          <div>
-            <p>Contact</p>
-            <p>Submit the form below to get in touch with me</p>
+        <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
+          <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Contact</p>
+            <p className='py-6'>Submit the form below to get in touch with me</p>
           </div>
 
-          <div>
-            <form action='' className='' >
+          <div className='flex justify-center items-center'>
+            <form action='https://getform.io/f/d2678bab-bb70-490e-bdb8-9efd91a917df' method='POST' className='flex flex-col w-full md:w-1/2' >
               <input 
                 type='text' 
                 name='name' 
@@ -21,10 +21,11 @@ const Contact = () => {
                 type='text' 
                 name='email' 
                 placeholder='Enter your email' 
-                className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' 
+                className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' 
               />
               <textarea 
                 name='message' 
+                placeholder='Enter your message'
                 rows={10} 
                 className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
               ></textarea>

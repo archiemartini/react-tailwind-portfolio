b6929ad27d05db02fea484060c8f889e7a9d02da
Archie Martin <archie.cgm@gmail.com>
2023-10-04 13:02:46 +0100

    captured current ref to remove warning

--- src/hooks/useFadeInOnScroll.js
+++ src/hooks/useFadeInOnScroll.js
@@ -5,6 +5,8 @@ const useFadeInOnScroll = () => {
   const elementRef = useRef(null);
 
   useEffect(() => {
+    const currentElement = elementRef.current; 
+
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
@@ -17,13 +19,13 @@ const useFadeInOnScroll = () => {
       }
     );
 
-    if (elementRef.current) {
-      observer.observe(elementRef.current);
+    if (currentElement) {
+      observer.observe(currentElement);
     }
 
     return () => {
-      if (elementRef.current) {
-        observer.unobserve(elementRef.current);
+      if (currentElement) {
+        observer.unobserve(currentElement);
       }
     };
   }, []);

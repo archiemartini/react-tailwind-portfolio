74cfd8e90241716d185af82a57edd780a73d3fc1
Archie Martin <archie.cgm@gmail.com>
2023-10-04 11:46:03 +0100

    pop up looking chilll

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -3,33 +3,30 @@ import React from 'react'
 const Popup = ({trigger, setTrigger, portfolio}) => {
   const { title, src, href, repo, summary, info, technologies } = portfolio
   return (trigger) ? (
-    <div className='popup w-full h-screen'>
-      <div className='popup-inner'>
-      <button className='close-btn' onClick={(e) => {
-          e.stopPropagation();
-          setTrigger(null);
-      }}>close</button>
-        <div className='text-1xl text-center font-bold pb-2'>{title}</div>
-        <img src={src} alt={title}   className='rounded-md'/>
+    <div className='popup flex justify-center items-center'>
+      <div className='popup-inner relative rounded-lg shadow-md'>
+        <button className='close-btn' onClick={(e) => {
+            e.stopPropagation();
+            setTrigger(null);
+        }}>&times;</button>
+        <div className='text-1xl text-center font-extrabold pb-2'>{title}</div>
+        <img src={src} alt={title} className='rounded-md w-full mb-4 shadow-lg'/>
         <div className='pt-2'>
           {summary && <p className='font-bold'>{summary}</p> }
           {info.map((line, index) => (
-            <p key={index}><span>- </span>{line}</p>
+            <p key={index} className='mb-1'><span>- </span>{line}</p>
           ))}
         </div>
-        <div className='font-bold pt-2'>
-          {href && <a className='underline' href={href}>Site</a> }
-          {href && repo && <span> / </span>}
-          {repo && <a className='underline' href={repo}>Repo</a> }
+        <div className='font-bold pt-2 flex justify-center space-x-4'>
+          {href && <a className='underline hover:text-blue-500' href={href}>Site</a>}
+          {repo && <a className='underline hover:text-blue-500' href={repo}>Repo</a>}
         </div>
-        <div className='text-center'>
-          Technologies used:
-        </div>
-        <div className='flex flex-wrap justify-center'>
+        <div className='text-center font-medium my-2'>Technologies used:</div>
+        <div className='flex flex-wrap justify-center gap-2'>
           {technologies.map((tech, index) => (
-            <div key={index} className='rounded-md m-0.5 py-1 px-2 bg-gradient-to-r from-cyan-500 to-blue-500 text-white'>
-             {tech}
-            </div>
+            <span key={index} className='bg-gray-200 rounded-md px-2 py-1 hover:bg-blue-500 hover:text-white transition cursor-default'>
+              {tech}
+            </span>
           ))}
         </div>
       </div>

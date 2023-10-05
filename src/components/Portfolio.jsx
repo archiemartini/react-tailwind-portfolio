c50ea22c9c3c3090d15d553e17fec3bba254d40d
Archie Martin <archie.cgm@gmail.com>
2023-10-05 09:17:04 +0100

    styling

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -92,7 +92,7 @@ const Portfolio = () => {
   ];
 
   return (
-    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 text-white md:h-screen'>
+    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 text-white md:h-screen pt-10'>
       <div ref={contentRef} className={`max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full z-0 transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
